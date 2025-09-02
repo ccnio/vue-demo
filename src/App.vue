e22fa9796bcd3906c2f461412aa508b6ccd862d6
lijianfeng_dxm <lijfbest@gmail.com>
2025-09-02 11:26:26 +0800

    登录路由

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { Button as VanButton } from 'vant'
-
 /**
  * 在 JavaScript/TypeScript 中，当导入路径指向一个目录（如 ./stores）时，模块系统会自动查找该目录下的 入口文件，默认优先级为：
 
@@ -8,60 +6,18 @@ index.ts（TypeScript 项目首选）
 index.js
 因此，./stores 实际上等价于 ./stores/index.ts。
  */
-import { useUserStore } from './stores'
-import { request } from './utils/request'
-import type { User } from './types/user'
-const getUser = () => {
-  request('patient/myUser', 'get', { test: 1 })
-}
-
-const login = () => {
-  request<User>('login/password', 'POST', {
-    mobile: '13211112222',
-    password: 'abc12345',
-  })
-    .then((res) => {
-      console.log('成功', res.data.account)
-    })
-    .catch((err) => {
-      console.log('失败', err)
-    })
-}
-
-const store = useUserStore()
 </script>
 
 <template>
-  <div>
-    App {{ store.user }}
-    <van-button
-      type="primary"
-      @click="
-        store.setUser({
-          id: '1',
-          token: '1',
-          mobile: '1',
-          avatar: '123',
-          account: '1',
-        })
-      "
-      >登录</van-button
-    >
-    <van-button type="primary" @click="store.delUser">退出</van-button>
-    <van-button type="primary" @click="getUser">请求</van-button>
-
-    <div><van-button type="default" @click="login">登录</van-button></div>
+  <div id="app">
+  <!--
+  1. 当用户点击<router-link>或通过代码导航时，URL发生变化
+  2. Vue Router根据新URL匹配对应的路由规则
+  3. 将匹配到的组件渲染到<router-view/>所在的位置
+  4. 旧组件会被销毁，新组件会被创建并挂载
+这个标签是Vue Router的核心组件，几乎所有Vue单页应用都必须使用它来实现页面内容的动态切换。  -->
+    <router-view />
   </div>
 </template>
 
-<style scoped>
-.main {
-  color: var(--cp-primary);
-  border-color: var(--footer-color);
-}
-
-.footer {
-  color: var(--cp-primary);
-  background-color: var(--footer-color);
-}
-</style>
+<style lang="scss" scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
   // createWebHistory参数为基础路径，在vite.config.ts中配置 base，默认值为 /，可以配置为 /project-name，这样路由就会在 /project-name 下工作
   // 路由模式有 hash、history，abstract 三种，hash 模式会在路径中添加 # 号，history 模式则不会
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{ path: '/login', component: () => import('@/views/Login/index.vue') }],
 })
 
 export default router
